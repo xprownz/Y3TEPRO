f77cf2a04bad47994e13e933a6886dc81973d490
RISK\ReiLaa01 <aaron.reilly@lexisnexis.com>
2019-11-06 19:54:56 +0000

    Connecting and fetching data from MongoDB

--- TheMateTricks/Backend/app.js
+++ TheMateTricks/Backend/app.js
@@ -1,6 +1,17 @@
 const express = require ('express');
 const bodyParser = require("body-parser");
+const Post = require('./models/post');
+const mongoose = require('mongoose');
 const app = express();
+
+//connection to mongoDB
+mongoose.connect('mongodb+srv://Aaron:eBIa0zCI1xLkdZlB@cluster0-yiwjf.mongodb.net/node-angular?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database')
+  })
+  .catch(() => {
+    console.log('Connection failed')
+  });
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,32 +22,30 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   next();
 })
+//eBIa0zCI1xLkdZlB
+//post object managed by mongoose
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  //saving to the database
+  post.save();
   res.status(201).json({
     message:'Post added successfully'
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: "kadfjalsgda2321",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "kadfjals14444",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-    res.status(200).json({
-      message:"Posts fetched successfully!",
-      posts: posts
+app.get('/api/posts', (req, res, next) => {
+  //return all results
+  Post.find()
+    .then(documents => {
+      console.log(documents);
+      res.status(200).json({
+        message:"Posts fetched successfully!",
+        posts: documents
+        });
     });
-
 });
 
 module.exports = app;
---
+++ TheMateTricks/Backend/models/post.js
@@ -0,0 +1,11 @@
+//import mongoose creating a schema
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  //Passing javascript object, title/content are required
+  title: { type: String, required: true },
+  content:{type: String, required: true }
+})
+
+//exporting the model for use outside of this file
+module.exports = mongoose.model('Post', postSchema);
