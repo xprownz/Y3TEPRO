9cccc310a01ec7a06639b364622d6e138cf883a6
Cianm <cianharry@gmail.com>
2019-11-08 18:34:28 +0000

    modified the post response data in posts.service.ts so that the id paramater of the post model matches on the front and back end

--- TheMateTricks/src/app/posts/posts.service.ts
+++ TheMateTricks/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 import { stringify } from '@angular/compiler/src/util';
@@ -13,9 +14,20 @@ export class PostsService {
   constructor(private http: HttpClient){}
 
   getTattooPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http.get<{message: string, posts: any }>(
+      'http://localhost:3000/api/posts'
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
         this.postsTattooUpdated.next([...this.posts]);
       });
   }
