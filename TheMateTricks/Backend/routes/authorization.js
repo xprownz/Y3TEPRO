6fb5ef45f710f9afdc22f48966a370cac7590d09
Cianm <cianharry@gmail.com>
2019-11-09 21:17:44 +0000

    Installed the bcrypt node package to allow us to encrypt the user's pasword for security and data protection reasons. Added the import to the authorization.js file in the imports folder

--- TheMateTricks/Backend/routes/authorization.js
+++ TheMateTricks/Backend/routes/authorization.js
@@ -1,13 +1,33 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+
 const Auth = require("../models/authorization");
 
-const app = express.Router();
+const router = express.Router();
 
 // creating the signup route that submit the authorization model to the mongoDB
 router.post("/signup", (req, res, next)  => {
-    const auth = new Auth({
-        email: req.body.email,
-        password: req.body.password
+    // using bcrypt node package to encrypt the user password and send the hash back as a response
+    bcrypt.hash(req.bod.password, 15)
+    .then(hash => {
+        const auth = new Auth({
+            email: req.body.email,   
+            password: hash
+        });
+        // save the user to the database with the encyption for security
+        auth.save()
+        .then(result => {
+            res.status(201).json({
+                message: "User has been created",
+                result: result
+            });
+        })
+        // catch is used for error handling for failed authentication
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
     });
 });
 
