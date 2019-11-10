5454ab593bc759765c45648bb1625fc8efaf3c54
Cianm <cianharry@gmail.com>
2019-11-10 21:24:44 +0000

    Created the login route in the authorization.js file, checks the DB for the email entered using the Auth Mongoose model. Installed the json web token node pacakage. This allows the creation of json token that can be added for authentiication. JWT is  added to the session after a succesful login and is stored on the server, providing security.

--- TheMateTricks/Backend/routes/authorization.js
+++ TheMateTricks/Backend/routes/authorization.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const Auth = require("../models/authorization");
 
@@ -27,6 +28,32 @@ router.post("/signup", (req, res, next)  => {
             res.status(500).json({
                 error: err
             });
+        });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+    Auth.findOne({ email: req.body.email }).then(user => {
+        if (!user) {
+            return res.status(401).json({
+                message: "User authentication failed"
+            });
+        }
+        return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+        if (!result) {
+            return res.status(401).json({
+                message: "User authentication failed"
+            });
+        } 
+        // the jwt.sign() method is what initializes the json web token 
+        // the second paramter is a 'secret' or unique password that is used to create the jwt, stored on the server (security)
+        const jwtoken = jwt.sign({email: user.email, userId: user._id}, 'kytdhfmfksglefdfmaklejieejg;adgkheghkghkgn/leajg;kah' );
+    })
+    .catch(err => {
+        return res.status(401).json({
+            message: "User authentication failed"
         });
     });
 });
