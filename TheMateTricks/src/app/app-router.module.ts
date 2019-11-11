abe98e9f6b705b0ef611dc8a42f02bd59c56e67e
Cianm <cianharry@gmail.com>
2019-11-11 19:27:11 +0000

    Empty commit

--- TheMateTricks/src/app/app-router.module.ts
+++ TheMateTricks/src/app/app-router.module.ts
@@ -9,8 +9,8 @@ import { CarouselComponent } from './carousel/carousel.component';
 const routes: Routes = [
     // the path is what appears after the domain, empty is the home/index page
     // the componemt parameter defines what component is loaded for the path specified
-    { path: '', component: CarouselComponent },
-    { path: 'browse', component: PostListComponent },
+    { path: '', component: PostListComponent },
+    { path: 'browse', component: CarouselComponent },
     { path: 'create', component: PostCreateComponent},
     { path: 'edit/:tattooId', component: PostCreateComponent},
     { path: 'login', component: LoginComponent },
